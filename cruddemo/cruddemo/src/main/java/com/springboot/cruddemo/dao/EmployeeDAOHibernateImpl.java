493da02664891ba0a6e27c29f4b11c26ea562919
moytri <moytritopa@gmail.com>
2019-01-29 12:27:45 -0800

    create dao and impl

---
+++ cruddemo/cruddemo/src/main/java/com/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -0,0 +1,48 @@
+package com.springboot.cruddemo.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.springboot.cruddemo.entity.Employee;
+
+@Repository
+public class EmployeeDAOHibernateImpl implements EmployeeDAO {
+
+	
+	//define field for entityManager
+	private EntityManager entityManager;
+	
+	//set up constructor injection
+	//entityManager is automatically created by Springboot
+	//automatically injected by Springboot constructor
+	@Autowired
+	public EmployeeDAOHibernateImpl(EntityManager theEntityManager) {
+		entityManager = theEntityManager;
+	}
+	
+	@Override
+	@Transactional
+	public List<Employee> findAll() {
+
+		//get the current hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+		
+		//create query
+		Query<Employee> theQuery = 
+				currentSession.createQuery("from Employee", Employee.class);
+		
+		//execute the query and get result list
+		List<Employee> employees = theQuery.getResultList();
+		
+		//return result
+		return employees;
+	}
+
+}
