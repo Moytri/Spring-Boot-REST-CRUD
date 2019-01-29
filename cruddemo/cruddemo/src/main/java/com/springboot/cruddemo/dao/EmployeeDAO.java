81abb7599af284aa7165dd88beec245c36077934
moytri <moytritopa@gmail.com>
2019-01-29 13:01:26 -0800

    include new crud methods

--- cruddemo/cruddemo/src/main/java/com/springboot/cruddemo/dao/EmployeeDAO.java
+++ cruddemo/cruddemo/src/main/java/com/springboot/cruddemo/dao/EmployeeDAO.java
@@ -5,5 +5,8 @@ import java.util.List;
 import com.springboot.cruddemo.entity.Employee;
 
 public interface EmployeeDAO {
-	public List<Employee> findAll();	
+	public List<Employee> findAll();
+	public Employee findById(int theId);
+	public void save(Employee theEmployee);
+	public void deleteById(int theId);
 }
